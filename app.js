6d12c56f2d6b8d64127f02d8081b5271926db0d8
StardustInfinity <twinkledas086@gmail.com>
2020-09-17 18:34:43 +0530

    Implemented local storage

--- app.js
+++ app.js
@@ -23,43 +23,35 @@ const twitterUsers = [
 
 (function() {
     let timeLine = document.querySelector('#tweet-display');
-    let tweetArr = localStorage.getItem('tweet').split(' ') || [];
+    let tweetArr = JSON.parse(localStorage.getItem('tweet')) || [];
     const tweetBtn = document.querySelector('#tweet-btn');
     const inputTweet = document.querySelector('#tweet-input');
     const inputSearch = document.querySelector('#input-search');
     const searchResult = document.querySelector('#search-result');
-    const deleteTweet = document.querySelector('.deleteBtn');
-
 
     function renderTweets() {
+        timeLine.innerHTML = ''
         if(localStorage.getItem('tweet') != '') {
-            const node = document.createElement('DIV');
-            const textnode = document.createTextNode(localStorage.getItem('tweet'));
-            node.appendChild(textnode);
-            timeLine.insertBefore(node, timeLine.firstChild);
-            deleteTweet.style.display = 'block';
+            tweetArr.forEach((tweet) => {
+                const node = document.createElement('DIV');
+                node.innerText = tweet.value;
+                timeLine.insertBefore(node, timeLine.firstChild);})
         } else {
-            timeLine.innerText = '';
+            const node = document.createElement('DIV');
+            node.innerText = inputTweet.value;
+            timeLine.insertBefore(node, timeLine.firstChild);
         }
-
     }
-
-    // tweetBtn.addEventListener('click', function() {
-    //     const node = document.createElement('DIV');
-    //     const textnode = document.createTextNode(inputTweet.value);
-    //     node.appendChild(textnode);
-    //     timeLine.insertBefore(node, timeLine.firstChild);
-    //     localStorage.setItem('tweet', inputTweet.value);
-    //     inputTweet.value = '';
-    //     tweetBtn.classList.remove('active');
-    //     tweetBtn.disabled = true;
-    // });
 
     renderTweets();
 
     tweetBtn.addEventListener('click', function() {
-        tweetArr.push(inputTweet.value);
-        localStorage.setItem('tweet', tweetArr);
+        tweetArr.push({
+            id: tweetArr.length + 1,
+            value: inputTweet.value
+        })
+        localStorage.setItem('tweet', JSON.stringify(tweetArr));
+        console.log(tweetArr)
         renderTweets();
         inputTweet.value = '';
         tweetBtn.classList.remove('active');
@@ -76,15 +68,6 @@ const twitterUsers = [
             tweetBtn.disabled = true;
         }
     });
-
-    deleteTweet.addEventListener('click', function() {
-        alert('Are you sure you want to delete?');
-        if(localStorage.getItem('tweet')) {
-            localStorage.setItem('tweet', []);
-            renderTweets();
-            deleteTweet.style.display = 'none';
-        }
-    })
 
     inputSearch.addEventListener('keyup', function(event) {
         let searchQuery = event.target.value;
@@ -103,5 +86,4 @@ const twitterUsers = [
             searchResult.classList.remove('visible');
         }
     })
-
 })();
